cf93c2268d5f6c0b9d617f7468c44387f666a42b
ckhachroo <chittesh.khachroo@goto.com>
2024-08-17 00:30:02 +0530

    igniting the app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
-  { id: "parent" },
+  { id: "parents" },
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "siblings" }, "Siblings"),
     React.createElement("h2", {}, "sibling2"),
