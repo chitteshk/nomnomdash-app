f63215ae0124f92541517eabad12f3826000873e
ckhachroo <chittesh.khachroo@goto.com>
2024-08-16 22:31:22 +0530

    introduction on how to use REACT and REACTDOM properties to render elements

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "siblings" }, "Siblings"),
+    React.createElement("h2", {}, "sibling2"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
